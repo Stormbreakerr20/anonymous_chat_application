7ce20fb2a76fc9d760c5d26549380c38f4d885be
nitin517 <nitinchaurasiya1749@gmail.com>
2025-01-26 19:33:32 +0530

    feature/(errors)

--- anonymous_chat_application-master/server/controllers/channelController.js
+++ anonymous_chat_application-master/server/controllers/channelController.js
@@ -2,13 +2,15 @@ const Channel = require('../models/Channel');
 const Message = require('../models/Message');
 const Users = require('../models/UserModel');
 const cloudinary = require('../config/cloudinaryConfig');
+const axios = require('axios'); // Import Axios for making HTTP requests
+const ConfessionMessage = require('../models/ConfessionMessage'); 
 
 const mongoose = require('mongoose');
 // Fetch all channels
 exports.getAllChannels = async (req, res) => {
     try {
         const channels = await Channel.find();
-        res.status(200).json({ success: true, channels });
+        res.status(200).json({ success: true, channels});
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
@@ -23,10 +25,16 @@ exports.getChannelMessages = async (req, res) => {
         if (!mongoose.Types.ObjectId.isValid(channelId)) {
             return res.status(400).json({ success: false, message: "Invalid Channel ID" });
         }
-
-        const messages = await Message.find({ channelId: new mongoose.Types.ObjectId(channelId) }).populate('userId', 'name');
-
-        res.status(200).json({ success: true, messages });
+         // Find the channel by ID
+        const channel = await Channel.findById(channelId);
+        if (!channel) {
+            return res.status(404).json({ success: false, message: "Channel not found" });
+        }
+        // Checking if we have to render the mention option in the ui or not
+        const isConfession = channel.name === 'Confessions';
+        const messages = await Message.find({ channelId: new mongoose.Types.ObjectId(channelId) })
+            .populate('userId', 'name');
+        res.status(200).json({ success: true, isConfession, messages });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
@@ -50,7 +58,27 @@ exports.postMessage = async (req, res) => {
     const { userId,content,imageUrl } = req.body; // Assume userId is passed via the frontend
 
     try {
-        const newMessage = await Message.create({ channelId,userId, content,imageUrl });
+        // const newMessage = await Message.create({ channelId,userId, content,imageUrl });
+        // res.status(201).json({ success: true, message: newMessage });
+         const channel = await Channel.findById(channelId);
+        if (!channel) {
+            return res.status(404).json({ success: false, message: "Channel not found" });
+        }
+        if (channel.name === 'Confessions' && mention) {
+            // Using the Confession Message schema to save the message
+            const newGeneralMessage = await GeneralMessage.create({ channelId, userId, content });
+            // Make a POST request to the external URL with the message data
+            await axios.post(`${process.env.URL}/api/dm`, {
+                channelId: newGeneralMessage.channelId,
+                userId: newGeneralMessage.isMention,
+                content: newGeneralMessage.content,
+                imageUrl: newGeneralMessage.imageUrl,
+                createdAt: Date.now
+            });
+            res.status(201).json({ success: true, message: newGeneralMessage });
+        }
+        // For other channels, use the regular Message schema
+        const newMessage = await Message.create({ channelId, userId, content,imageUrl });
         res.status(201).json({ success: true, message: newMessage });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
